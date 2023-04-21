e609638a8aff1a09940e57b1cbf5ed975b4650f9
Nguyen Minh Ngoc <ngoc.nguyenminhhcmut@gmail.com>
2023-04-21 18:00:26 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/9igesOS5Dt85Z4MG/scene.splinecode" />
+  );
+}

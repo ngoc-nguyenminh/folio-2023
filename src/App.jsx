0735e312b3e6d6ae217902729a9841978f2af29d
Nguyen Minh Ngoc <ngoc.nguyenminhhcmut@gmail.com>
2023-05-13 12:00:34 +0700

    mini house

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/9igesOS5Dt85Z4MG/scene.splinecode" />
+    // <Spline scene="https://prod.spline.design/9igesOS5Dt85Z4MG/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/CQCn7onV7FGIzMTz/scene.splinecode" />
   );
 }
